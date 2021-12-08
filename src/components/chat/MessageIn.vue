5dbb21dd2bb8cdb112560540978ad275bf3a782b
PibedeBarr10 <root@DESKTOP-LRBA7I4.localdomain>
2021-12-08 14:15:59 +0100

    update

--- src/components/chat/MessageIn.vue
+++ src/components/chat/MessageIn.vue
@@ -1,6 +1,11 @@
 <template>
       <div class="flex items-end w-1/2 m-2 ">
-        <img class="w-8 rounded-full m-3 mb-0" :src="avatar" alt="avatar" />
+        <img
+          class="w-8 rounded-full m-3 mb-0"
+          :src="avatar"
+          alt="avatar"
+          style="max-width: 2rem; max-height: 2rem;"
+        />
         <div class="p-3  bg-gray-100 rounded-t-lg rounded-br-lg">
           <div class="text-sm text-gray-500 ">
             {{ text }}
